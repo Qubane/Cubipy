2fe7f671b211d67bb558ff8ad58435de298932a8
UltraQbik <no1skill@yandex.ru>
2025-03-11 16:07:49 +0300

    Working mandelbrot test

--- assets/shaders/main.glsl
+++ assets/shaders/main.glsl
@@ -3,20 +3,22 @@
 
 int iterate_pos(vec2 coord) {
     vec2 z = coord;
-    float zmagx, zmagy;
+    vec2 sq;
     for (int i = 0; i < MAX_ITERATIONS; i++) {
-        zmagx = z.x * z.x;
-        zmagy = z.y * z.y;
-        if (zmagx + zmagy > 4) {
+        sq = z * z;
+        if (sq.x + sq.y > 4)
             return i;
-        }
-        z = vec2(zmagx - zmagy + z.x, 2 * z.x * z.y + z.y);
+        z = vec2(sq.x - sq.y + coord.x, 2.f * z.x * z.y + coord.y);
     }
-    return MAX_ITERATIONS;
+    return 0;
 }
 
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
-    vec2 normalizedFragCoord = fragCoord / iResolution.xy;
-    fragColor = vec4(fragCoord, 0, 0);
+    vec2 uv = (fragCoord - iResolution.xy * 0.5f) / iResolution.y;
+    uv *= 2;
+
+    float value = float(iterate_pos(uv - vec2(0.5f, 0.f))) / MAX_ITERATIONS;
+
+    fragColor = vec4(value);
 }
