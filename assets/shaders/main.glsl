462b48c4fa4dba0efa111444038cd75754dfee14
UltraQbik <no1skill@yandex.ru>
2025-03-11 15:10:18 +0300

    Add iterate pos thing

--- assets/shaders/main.glsl
+++ assets/shaders/main.glsl
@@ -1,3 +1,21 @@
+#define MAX_ITERATIONS 128
+
+
+int iterate_pos(vec2 coord) {
+    vec2 z = coord;
+    float zmagx, zmagy;
+    for (int i = 0; i < MAX_ITERATIONS; i++) {
+        zmagx = z.x * z.x;
+        zmagy = z.y * z.y;
+        if (zmagx + zmagy > 4) {
+            return i;
+        }
+        z = vec2(zmagx - zmagy + z.x, 2 * z.x * z.y + z.y);
+    }
+    return MAX_ITERATIONS;
+}
+
+
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     vec2 normalizedFragCoord = fragCoord / iResolution.xy;
     fragColor = vec4(fragCoord, 0, 0);
