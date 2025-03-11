86fddbf1811b97627aaa3994211b111c197b3860
UltraQbik <no1skill@yandex.ru>
2025-03-11 12:18:01 +0300

    Use Window class

--- source/application.py
+++ source/application.py
@@ -3,13 +3,20 @@ Application class
 """
 
 
+from source.window import Window
+
+
 class Application:
     """
     Application class
     """
 
-    def __init__(self):
-        ...
+    def __init__(
+            self,
+            win_width: int = 1280,
+            win_height: int = 720,
+            win_title: str = "window"):
+        self.window: Window = Window(win_width, win_height, win_title)
 
     def run(self):
         ...
