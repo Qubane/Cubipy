6f3f56ec4bb7096a7ffc8ec0feeb12b83d89b40c
UltraQbik <no1skill@yandex.ru>
2025-03-12 22:45:55 +0300

    Chunk class

--- source/world.py
+++ source/world.py
@@ -1,3 +1,17 @@
 """
 World related operations
 """
+
+
+import numpy as np
+from source.options import *
+
+
+class Chunk:
+    """
+    16x16x16 block chunk container
+    """
+
+    def __init__(self, position: tuple[int, int, int]):
+        self.position: tuple[int, int, int] = position
+        self.voxels: np.ndarray = np.zeros([CHUNK_SIZE, CHUNK_SIZE, CHUNK_SIZE], dtype=np.uint32)
