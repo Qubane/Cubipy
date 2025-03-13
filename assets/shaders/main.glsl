8d8b1b42e04de7e3ccdd15fab37d9490f71dfed7
UltraQbik <no1skill@yandex.ru>
2025-03-13 11:39:51 +0300

    idk

--- assets/shaders/main.glsl
+++ assets/shaders/main.glsl
@@ -1,14 +1,12 @@
-struct Voxel {
-    uint id;
-};
-
-
-uniform Voxel CHUNK_DATA[];
 uniform uint CHUNK_SIZE;
+uniform uint CHUNK_DATA[16*16*16];
 
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     vec2 uv = (fragCoord - iResolution.xy * 0.5f) / iResolution.y;
+
+    CHUNK_SIZE;  // keep uniforms active
+    CHUNK_DATA[0];  // keep uniforms active
 
     fragColor = vec4(uv, 0, 0);
 }
