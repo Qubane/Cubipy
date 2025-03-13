b447eefa9c0a13a785f5964f7067f9a64cc7548c
UltraQbik <no1skill@yandex.ru>
2025-03-13 17:09:23 +0300

    Separate cast_ray and get_voxel

--- assets/shaders/main.glsl
+++ assets/shaders/main.glsl
@@ -1,31 +1,31 @@
 #define CHUNK_SIZE 16
-#define CHUNK_LAYER CHUNK_SIZE * CHUNK_SIZE
-#define CHUNK_ROW CHUNK_SIZE
-#define STEP_SIZE 0.05f
+#define STEP_SIZE 0.01f
 
 
-uniform uint CHUNK_DATA[CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE];
+uniform int CHUNK_DATA[CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE];
+
+
+int get_voxel(ivec3 pos) {
+    if (pos.x > -1 && pos.x < CHUNK_SIZE &&
+        pos.y > -1 && pos.y < CHUNK_SIZE &&
+        pos.z > -1 && pos.z < CHUNK_SIZE) {
+        return CHUNK_DATA[pos.z * CHUNK_SIZE * CHUNK_SIZE + pos.y * CHUNK_SIZE + pos.x];
+    }
+    return -1;
+}
 
 
 vec3 cast_ray(vec3 origin, vec3 direction) {
-    ivec3 position;
     float d;
     while (true) {
-        position = ivec3(origin);
-        if (position.x > -1 && position.x < CHUNK_SIZE &&
-            position.y > -1 && position.y < CHUNK_SIZE &&
-            position.z > -1 && position.z < CHUNK_SIZE) {
-            if (CHUNK_DATA[int(position.z * CHUNK_LAYER + position.y * CHUNK_ROW + position.x)] > 0.f) {
-                return origin;
-            }
-        }
+        if (get_voxel(ivec3(origin)) > 0)
+            return origin;
 
         origin += direction * STEP_SIZE;
         d += STEP_SIZE;
 
-        if (d > 32.f) {
+        if (d > 32.f)
             return origin;
-        }
     }
 }
 
