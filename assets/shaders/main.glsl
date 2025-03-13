53f98b554682bb572b4c3411fc5b535c8d207db7
UltraQbik <no1skill@yandex.ru>
2025-03-13 17:29:44 +0300

    Change ray casting to use DDA algorithm

--- assets/shaders/main.glsl
+++ assets/shaders/main.glsl
@@ -1,5 +1,11 @@
 #define CHUNK_SIZE 16
-#define STEP_SIZE 0.01f
+
+
+struct CollisionInfo {
+    vec3 position;
+    float dist;
+    int voxel_id;
+};
 
 
 uniform int CHUNK_DATA[CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE];
@@ -15,17 +21,70 @@ int get_voxel(ivec3 pos) {
 }
 
 
-vec3 cast_ray(vec3 origin, vec3 direction) {
-    float d;
+CollisionInfo cast_ray(vec3 origin, vec3 direction) {
+    ivec3 ray_pos, ray_step;
+    vec3 ray_unit_step, ray_length;
+    float dist;
+
+    // calculate integer ray positon and unit step
+    ray_pos = ivec3(origin);
+    ray_unit_step = abs(1.f / direction);
+
+    // calculate steps and starting lengths
+    if (direction.x > 0) {
+        ray_step.x = 1;
+        ray_length.x = (float(ray_pos.x) - origin.x + 1) * ray_unit_step.x;
+    } else {
+        ray_step.x = -1;
+        ray_length.x = (origin.x - float(ray_pos.x)) * ray_unit_step.x;
+    }
+    if (direction.y > 0) {
+        ray_step.y = 1;
+        ray_length.y = (float(ray_pos.y) - origin.y + 1) * ray_unit_step.y;
+    } else {
+        ray_step.y = -1;
+        ray_length.y = (origin.y - float(ray_pos.y)) * ray_unit_step.y;
+    }
+    if (direction.z > 0) {
+        ray_step.z = 1;
+        ray_length.z = (float(ray_pos.z) - origin.z + 1) * ray_unit_step.z;
+    } else {
+        ray_step.z = -1;
+        ray_length.z = (origin.z - float(ray_pos.z)) * ray_unit_step.z;
+    }
+
+    // cast ray
+    int voxel_id;
     while (true) {
-        if (get_voxel(ivec3(origin)) > 0)
-            return origin;
+        // check for block collision
+        voxel_id = get_voxel(ray_pos);
+        if (voxel_id > 0)
+            return CollisionInfo(
+                direction * dist,
+                dist,
+                voxel_id);
 
-        origin += direction * STEP_SIZE;
-        d += STEP_SIZE;
+        // make a step
+        if (ray_length.x < ray_length.y && ray_length.x < ray_length.z) {
+            ray_pos.x += ray_step.x;
+            dist = ray_length.x;
+            ray_length.x += ray_unit_step.x;
+        } else if (ray_length.y < ray_length.z) {
+            ray_pos.y += ray_step.y;
+            dist = ray_length.y;
+            ray_length.y += ray_unit_step.y;
+        } else {
+            ray_pos.z += ray_step.z;
+            dist = ray_length.z;
+            ray_length.z += ray_unit_step.z;
+        }
 
-        if (d > 32.f)
-            return origin;
+        // check for length; if too far then return
+        if (dist > 64.f)
+            return CollisionInfo(
+                direction * dist,
+                dist,
+                voxel_id);
     }
 }
 
@@ -35,7 +94,7 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
 
     vec3 origin = vec3(8);
     vec3 direction = normalize(vec3(uv.x, 0.5f, uv.y));
-    vec3 block_pos = cast_ray(origin, direction);
+    CollisionInfo casted_ray = cast_ray(origin, direction);
 
-    fragColor = vec4(length(block_pos) / 48);
+    fragColor = vec4(casted_ray.dist / 48);
 }
