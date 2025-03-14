2ee70e2894549862399e2f8f1a28962353ccf529
UltraQbik <no1skill@yandex.ru>
2025-03-14 22:30:10 +0300

    Add AOV to FOV

--- source/classes.py
+++ source/classes.py
@@ -3,14 +3,15 @@ Classes definitions
 """
 
 
-from typing import Iterable
+import math
 from arcade import Vec2, Vec3
 
 
 class Player:
-    def __init__(self, position: Vec3, rotation: Vec2):
+    def __init__(self, position: Vec3, rotation: Vec2, aov: float = 90):
         self.pos: Vec3 = position
         self.rot: Vec2 = rotation
+        self.fov: float = 2 * math.tan(math.radians(aov) / 2)
 
     def move(self, offset: Vec3):
         self.pos += offset
