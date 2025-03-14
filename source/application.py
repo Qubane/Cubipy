7328ef524bbf297c219001e9989b13b75a5a704b
UltraQbik <no1skill@yandex.ru>
2025-03-14 22:42:15 +0300

    Add shitty keyboard movement

--- source/application.py
+++ source/application.py
@@ -3,8 +3,12 @@ Application class
 """
 
 
+import math
 import arcade
+from arcade import Vec2, Vec3
+from pyglet.event import EVENT_HANDLE_STATE
 from source.world import *
+from source.classes import *
 from source.options import *
 
 
@@ -30,9 +34,6 @@ class Application(arcade.Window):
         self.shadertoy: arcade.experimental.Shadertoy | None = None
         self.load_shaders()
 
-        # temp
-        self.chunk: Chunk = generate_debug(0.2)
-
         # make graphs
         arcade.enable_timings()
         self.perf_graph_list = arcade.SpriteList()
@@ -40,6 +41,15 @@ class Application(arcade.Window):
         graph = arcade.PerfGraph(200, 120, graph_data="FPS")
         graph.position = 100, self.height - 60
         self.perf_graph_list.append(graph)
+
+        # temp
+        self.chunk: Chunk = generate_debug(0.1)
+
+        # player
+        self.player: Player = Player(Vec3(8, 8, 8), Vec2(0, 0))
+
+        # controls
+        self.keys: set[int] = set()
 
     def load_shaders(self):
         """
@@ -52,14 +62,37 @@ class Application(arcade.Window):
             window_size,
             f"{SHADER_DIR}/main.glsl")
 
+    # noinspection PyTypeChecker
     def on_draw(self):
         self.use()
         self.clear()
 
         self.shadertoy.program.set_uniform_safe("CHUNK_SIZE", CHUNK_SIZE)
-        # noinspection PyTypeChecker
+        self.shadertoy.program.set_uniform_safe("PLR_FOV", self.player.fov)
+        self.shadertoy.program.set_uniform_array_safe("PLR_POS", self.player.pos)
+        self.shadertoy.program.set_uniform_array_safe("PLR_DIR", self.player.rot)
         self.shadertoy.program.set_uniform_array_safe("CHUNK_DATA", self.chunk.voxels.flatten())
 
         self.shadertoy.render()
 
         self.perf_graph_list.draw()
+
+    def on_key_press(self, symbol: int, modifiers: int) -> EVENT_HANDLE_STATE:
+        self.keys.add(symbol)
+
+    def on_key_release(self, symbol: int, modifiers: int) -> EVENT_HANDLE_STATE:
+        self.keys.discard(symbol)
+
+    def on_update(self, delta_time: float):
+        # keyboard movement
+        looking_direction: Vec2 = Vec2(math.sin(self.player.rot[1]), math.cos(self.player.rot[1]))
+        movement = looking_direction * delta_time * self.player.movement_speed
+
+        if arcade.key.W in self.keys:
+            self.player.move(Vec3(movement.x, movement.y, 0))
+        if arcade.key.S in self.keys:
+            self.player.move(Vec3(-movement.x, -movement.y, 0))
+        if arcade.key.A in self.keys:
+            self.player.move(Vec3(-movement.y, movement.x, 0))
+        if arcade.key.D in self.keys:
+            self.player.move(Vec3(movement.y, -movement.x, 0))
