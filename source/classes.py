6e00ba9dc57ffd5ae5f987b0596775e1aeeafb3e
UltraQbik <no1skill@yandex.ru>
2025-03-14 21:25:44 +0300

    Add Player class

--- source/classes.py
+++ source/classes.py
@@ -1,3 +1,19 @@
 """
 Classes definitions
 """
+
+
+from typing import Iterable
+from arcade import Vec2, Vec3
+
+
+class Player:
+    def __init__(self, position: Vec3, rotation: Vec2):
+        self.pos: Vec3 = position
+        self.rot: Vec2 = rotation
+
+    def move(self, offset: Vec3):
+        self.pos += offset
+
+    def rotate(self, offset: Vec2):
+        self.rot += offset
