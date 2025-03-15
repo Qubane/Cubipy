600ecf87e233ae83e02980b93d8d3e9bc0784d01
UltraQbik <no1skill@yandex.ru>
2025-03-15 21:33:54 +0300

    Add ChunkManager class

--- source/rendering.py
+++ source/rendering.py
@@ -1,3 +1,18 @@
 """
 Some of the rendering related things are put here
 """
+
+
+from source.world import *
+from source.classes import *
+
+
+class ChunkManager:
+    """
+    Manages chunk related things
+    """
+
+    player: Player | None = None
+
+    def __init__(self, world: World):
+        self.world: World = world
