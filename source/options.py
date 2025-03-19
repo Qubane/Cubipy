0678da482bfc668494debbfd5c620a9ec05ef047
UltraQbik <no1skill@yandex.ru>
2025-03-19 14:52:39 +0300

    Add SAVES_DIR

--- source/options.py
+++ source/options.py
@@ -6,6 +6,8 @@ Configuration file
 ASSETS_DIR = "assets"
 SHADER_DIR = f"{ASSETS_DIR}/shaders"
 
+SAVES_DIR = "saves"
+
 WORLD_SIZE = 512
 WORLD_LAYER = WORLD_SIZE ** 2
 WORLD_CENTER = WORLD_SIZE // 2
