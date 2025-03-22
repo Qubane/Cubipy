3a2c2c9daa6ba78924011eaa33942cd8374c0601
UltraQbik <no1skill@yandex.ru>
2025-03-22 09:49:23 +0300

    Add WINDOW_RESOLUTION

--- source/options.py
+++ source/options.py
@@ -13,4 +13,5 @@ WORLD_SIZE: int = 512
 WORLD_LAYER: int = WORLD_SIZE ** 2
 WORLD_CENTER: int = WORLD_SIZE // 2
 
+WINDOW_RESOLUTION: tuple[int, int] = (16 * 90, 9 * 90)
 SCREENSHOT_RESOLUTION: tuple[int, int] = (3840, 2160)
