87240a1c36eaff06892cb851cdcfeacb4ca56b0f
UltraQbik <no1skill@yandex.ru>
2025-03-25 07:44:25 +0300

    Add __init__ method

--- source/textures.py
+++ source/textures.py
@@ -11,3 +11,9 @@ class TextureManager:
     """
     Manages textures
     """
+
+    def __init__(self):
+        self.texture_list: list[arcade.context.Texture2D] = []
+        self._named_mapping: dict[str, dict[str, int]] = {}
+
+        self.block_texture_mapping: dict[int, dict[str, int | dict]] = {}
