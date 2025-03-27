648f101dc582c4783187e53f436df7d624b3c7d3
UltraQbik <no1skill@yandex.ru>
2025-03-27 15:55:59 +0300

    Garbaje looking sky

--- assets/shaders/sky.glsl
+++ assets/shaders/sky.glsl
@@ -1,4 +1,5 @@
 #version 430
+#define GRAD_LEN 5
 
 
 // shader output
@@ -11,14 +12,30 @@ uniform vec3 u_resolution;
 uniform vec2 u_playerDirection;
 
 // sky uniforms
-uniform int u_skyGradient[5];
+uniform int u_skyGradient[GRAD_LEN];
 
 // world uniforms
 uniform vec3 u_worldSun;
 
 
+vec3 getColor(int index) {
+    return vec3(
+        float(u_skyGradient[index] >> 16) / 255.f,
+        float((u_skyGradient[index] >> 8) & 0xff) / 255.f,
+        float(u_skyGradient[index] & 0xff)) / 255.f;
+}
+
+
 void main() {
     vec2 uv = (gl_FragCoord.xy - u_resolution.xy * 0.5f) / u_resolution.y;
+    float t = gl_FragCoord.y / u_resolution.y;
 
-    fragColor = vec4(uv, 0, 1);
+    int skySliceIndex = int(t * GRAD_LEN);
+
+    vec3 startColor = getColor(skySliceIndex);
+    vec3 endColor = getColor(skySliceIndex + 1);
+
+    float color_t = mod(t, 1.f / GRAD_LEN) * GRAD_LEN;
+
+    fragColor = vec4(vec3(startColor * (1 - color_t) + endColor * color_t), 1.f);
 }
