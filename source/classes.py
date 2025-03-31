30ab978cf3869743118a4f4e75fdd07601934661
UltraQbik <no1skill@yandex.ru>
2025-03-31 22:25:15 +0300

    Update speed

--- source/classes.py
+++ source/classes.py
@@ -13,7 +13,7 @@ class Player:
         self.rot: Vec2 = rotation
         self.fov: float = 1 / math.tan(math.radians(aov) / 2)
 
-        self.movement_speed: float = 32
+        self.movement_speed: float = 16
         self.sensitivity: float = 0.001
 
     def move(self, offset: Vec3):
